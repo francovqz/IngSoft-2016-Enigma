06cbc98eb8d0c2676dab7fdc998cd937941160da
Marco Heredia <marcoheredia@hotmail.com.ar>
2016-06-17 17:25:04 -0300

    Corrijo JUnit Test

--- src/test/java/headfirst/combined/djview/MyAnotadorControllerTest.java
+++ src/test/java/headfirst/combined/djview/MyAnotadorControllerTest.java
@@ -6,56 +6,52 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class MyAnotadorControllerTest {
-    @Before
-    public void method(){
-        System.setProperty("java.awt.headless", "false");
-    }
 
     @Test
     public void increaseBPM() throws Exception {
-        MyAnotadorModel clase= new MyAnotadorModel();
-        MyAnotadorController controlador= new MyAnotadorController(clase);//
-        clase.setJUG2(4);
+        MyAnotadorModel clase1= new MyAnotadorModel();
+        MyAnotadorController controlador= new MyAnotadorController(clase1);
+        clase1.setJUG2(4);
         controlador.increaseBPM();
-        assertEquals(5,clase.getJUG2());
+        assertEquals(5,clase1.getJUG2());
     }
     @Test
     public void increaseBPM2() throws Exception {
-        MyAnotadorModel clase= new MyAnotadorModel();
-        MyAnotadorController controlador= new MyAnotadorController(clase);//
-        clase.setJUG2(-3);
+        MyAnotadorModel clase2= new MyAnotadorModel();
+        MyAnotadorController controlador= new MyAnotadorController(clase2);
+        clase2.setJUG2(-3);
         controlador.increaseBPM();
-        assertEquals(-2,clase.getJUG2());
+        assertEquals(-2,clase2.getJUG2());
     }
 
     @Test
     public void decreaseBPM() throws Exception {
-        MyAnotadorModel clase= new MyAnotadorModel();
-        MyAnotadorController controlador= new MyAnotadorController(clase);//
-        clase.setJUG1(3);
+        MyAnotadorModel clase3= new MyAnotadorModel();
+        MyAnotadorController controlador= new MyAnotadorController(clase3);
+        clase3.setJUG1(3);
         controlador.decreaseBPM();
-        assertEquals(4,clase.getJUG1());
+        assertEquals(4,clase3.getJUG1());
     }
     @Test
     public void decreaseBPM2() throws Exception {
-        MyAnotadorModel clase= new MyAnotadorModel();
-        MyAnotadorController controlador= new MyAnotadorController(clase);//
-        clase.setJUG1(-1);
+        MyAnotadorModel clase4= new MyAnotadorModel();
+        MyAnotadorController controlador= new MyAnotadorController(clase4);
+        clase4.setJUG1(-1);
         controlador.decreaseBPM();
-        assertEquals(0,clase.getJUG1());
+        assertEquals(0,clase4.getJUG1());
     }
 
     @Test
     public void setBPM() throws Exception {
-        MyAnotadorModel clase= new MyAnotadorModel();
-        MyAnotadorController controlador= new MyAnotadorController(clase);//
+        MyAnotadorModel clase5= new MyAnotadorModel();
+        MyAnotadorController controlador= new MyAnotadorController(clase5);
         controlador.setBPM(30);
-        assertEquals(30,clase.getBPM());
+        assertEquals(30,clase5.getBPM());
     }
     @Test
     public void metodosInterface() throws Exception {
-        MyAnotadorModel clase= new MyAnotadorModel();
-        MyAnotadorController controlador= new MyAnotadorController(clase);
+        MyAnotadorModel clase6= new MyAnotadorModel();
+        MyAnotadorController controlador= new MyAnotadorController(clase6);
         controlador.view.updateBPM();
         controlador.view.updateBeat();
         controlador.view.updateJUG1();
@@ -64,14 +60,14 @@ public class MyAnotadorControllerTest {
         controlador.view.registrar();
         controlador.start();
         controlador.stop();
-        assertEquals(clase,controlador.model);
+        assertEquals(clase6,controlador.model);
     }
 
     @Test
     public void constructor() throws Exception {
-        MyAnotadorModel clase= new MyAnotadorModel();
-        MyAnotadorController controlador= new MyAnotadorController(clase);
-        MyAnotadorController controlador2= new MyAnotadorController(clase,controlador.view);
+        MyAnotadorModel clase7= new MyAnotadorModel();
+        MyAnotadorController controlador= new MyAnotadorController(clase7);
+        MyAnotadorController controlador2= new MyAnotadorController(clase7,controlador.view);
         assertEquals(controlador.view,controlador2.view);
     }
 }
